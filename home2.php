daf68da545cf37a1223646faefadbd89d97626e9
mohammed321co <74262750+mohammed321co@users.noreply.github.com>
2020-11-10 21:00:38 +0300

    added

---
+++ home2.php
@@ -0,0 +1,24 @@
+
+<html>
+<head>
+    <title> Home Page </title>
+         <link rel="stylesheet" type="text/css" href="css/style.css"/>
+         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+</head>
+
+<body>
+
+    <div class="container">
+    <a class="float-right" href="home.php">Logout </a>
+    </div>
+    <div class="container">
+    <a class="float-right" href="Profile.php">Profile</a>
+    </div>
+    <a class="float-right" href="cart.php">Cart</a>
+    <h1> Welcome </h1>
+
+
+
+</body>
+
+</html>
---
+++ home3.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+    <title> Home Page </title>
+         <link rel="stylesheet" type="text/css" href="css/style.css"/>
+         <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+</head>
+
+<body>
+
+    <div class="container">
+    <a class="float-right" href="home.php">Logout </a>
+    </div>
+    <a class="float-right" href="add.php">Add Product</a>
+    <h1> Welcome </h1>
+
+
+
+</body>
+
+</html>
